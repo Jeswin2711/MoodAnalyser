64a9ad72bb37a1b1025631a271e1301ef370122f
Jeswin2711 <jeswin2711@gmail.com>
2022-07-28 22:28:00 +0530

    Added : UseCase3

--- src/main/java/com/assignment/MoodAnalyserMain.java
+++ src/main/java/com/assignment/MoodAnalyserMain.java
@@ -4,12 +4,47 @@ import java.util.Scanner;
 
 public class MoodAnalyserMain
 {
+    /*
+    Enums Added NULL_ERROR and EMPTY_ERROR
+     */
+    public static final String NULL_ERROR = "NULL MOOD";
+    public static final String EMPTY_ERROR = "EMPTY MOOD ";
+
     public static void main(String[] args) {
         System.out.println("Welcome to Mood Analyser Problem");
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Your Mood");
-        String mood = sc.nextLine();
-        MoodAnalyser moodAnalyser = new MoodAnalyser(mood);
-        System.out.println(moodAnalyser.analyseMood());
+
+        /*
+        Using try Catch to Perform Exception handling
+         */
+        try {
+            System.out.println("Enter Your Mood");
+            String mood = sc.nextLine();
+            /*
+            if String is Empty throw Empty String Error
+             */
+            if (mood.isEmpty()) {
+                throw new MoodAnalysisException(EMPTY_ERROR);
+            }
+            /*
+            Check the user entered input is I am in Sad Mood or I am in Any Mood otherwise throw Exception
+             */
+
+            if(mood.equals("I am in Sad Mood") || mood.equals("I am in Any Mood") )
+            {
+                MoodAnalyser moodAnalyser = new MoodAnalyser(mood);
+                System.out.println(moodAnalyser.analyseMood());
+            }
+            else {
+                /*
+                Custom Exception
+                 */
+                throw new MoodAnalysisException(NULL_ERROR);
+            }
+        }
+        catch (Exception e)
+        {
+            System.out.println("Please Enter Valid Input " + e);
+        }
     }
 }
