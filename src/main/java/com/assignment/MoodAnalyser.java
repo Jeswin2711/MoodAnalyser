90525de6b685635a93416b0431aeaabc9c8c7275
Jeswin2711 <jeswin2711@gmail.com>
2022-07-28 18:32:51 +0530

    Added : UseCase1

--- src/main/java/com/assignment/MoodAnalyser.java
+++ src/main/java/com/assignment/MoodAnalyser.java
@@ -1,10 +1,31 @@
 package com.assignment;
 
+import java.util.Scanner;
+
 public class MoodAnalyser
 {
     public static void main(String[] args) {
         System.out.println("Welcome to Mood Analyser Problem");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Your Mood");
+        String mood = sc.nextLine();
+        System.out.println(analyseMood(mood));
+    }
 
+    /*
+ analyseMood function to return Mood is Happy or Sad
+  */
+    static String analyseMood(String msg)
+    {
+        String returnString = "";
+        if(msg.equals("I am in Sad Mood"))
+        {
+            returnString = "SAD";
+        }
+        else if(msg.equals("I am in Any Mood")) {
+            returnString = "HAPPY";
+        }
+        return returnString;
     }
 
 }
